94a5d3a77cc79095983fa8b4fcabf09d7684edc7
Timofey Chernasov <timofeychernasov@gmail.com>
2021-11-05 23:26:42 +0300

    Add test for task 2

--- src/test/java/ru/netology/web/DeliveryTest.java
+++ src/test/java/ru/netology/web/DeliveryTest.java
@@ -3,18 +3,19 @@ package ru.netology.web;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Condition.*;
 
 public class DeliveryTest {
+    private DayPlus dayPlus = new DayPlus();
+
     @Test
     void shouldFormDeliveryPositive() {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Владимир");
-        $("[data-test-id='date'] input").setValue(Date());
+        $("[data-test-id='date'] input").setValue(dayPlus.dayPlus(3));
         $("[data-test-id='name'] input").setValue("Сергей Сергеев");
         $("[data-test-id='phone'] input").setValue("+71234567890");
         $("[data-test-id='agreement']").click();
@@ -22,10 +23,22 @@ public class DeliveryTest {
         $("[data-test-id='notification'").shouldBe(appear, Duration.ofSeconds(15));
     }
 
-    public static String Date() {
-        LocalDate today = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String date = today.plusDays(3).format(formatter);
-        return date;
+    @Test
+    void shouldFormDeliveryPositiveWithList() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("аб");
+        $$(".menu-item__control").last().click();
+        $(".input__icon").click();
+        if (dayPlus.month()) {
+            $("[data-step='1']");
+        }
+        String day = dayPlus.day(7);
+        $$(".calendar__day").find(exactText(day)).click();
+        $("[data-test-id='name'] input").setValue("Сергей Сергеев");
+        $("[data-test-id='phone'] input").setValue("+71234567890");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id='notification'").shouldBe(appear, Duration.ofSeconds(15));
     }
+
 }
