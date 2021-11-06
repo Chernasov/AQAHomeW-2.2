65294c8828a6f40b9e719d1d1d1b268afa9ba64f
Timofey Chernasov <timofeychernasov@gmail.com>
2021-11-06 15:35:03 +0300

    Fix tests

--- src/test/java/ru/netology/web/DayPlus.java
+++ src/test/java/ru/netology/web/DayPlus.java
@@ -5,7 +5,6 @@ import java.time.format.DateTimeFormatter;
 
 public class DayPlus {
     LocalDate today = LocalDate.now();
-//    LocalDate today = LocalDate.of(2021, 12, 25); // проверял тест
 
     public String dayPlus(int plus) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
@@ -17,10 +16,10 @@ public class DayPlus {
         return String.valueOf(Integer.parseInt(today.plusDays(plus).format(formatter)));
     }
 
-    public boolean month() {
+    public boolean monthCheck(int plus) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM");
         int now = Integer.parseInt(today.format(formatter));
-        int forward = Integer.parseInt(today.plusDays(7).format(formatter));
+        int forward = Integer.parseInt(today.plusDays(plus).format(formatter));
         if (forward > now) {
             return true;
         }
